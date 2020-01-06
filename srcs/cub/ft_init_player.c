205eee0741728d16b464aceb013baca218cc84d4
Thomas Claudel <tclaudel@z2r1p6.le-101.fr>
2020-01-06 11:47:56 +0100

    orientation ok

--- srcs/cub/ft_init_player.c
+++ srcs/cub/ft_init_player.c
@@ -6,7 +6,7 @@
 /*   By: tclaudel <tclaudel@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2020/01/02 13:53:42 by tclaudel     #+#   ##    ##    #+#       */
-/*   Updated: 2020/01/06 11:11:12 by tclaudel    ###    #+. /#+    ###.fr     */
+/*   Updated: 2020/01/06 11:47:28 by tclaudel    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -16,16 +16,6 @@
 
 static void		ft_initvdir(t_cub *cub)
 {
-	if (cub->orientation == 'N')
-	{
-		cub->dir.x = 0;
-		cub->dir.y = 1;
-	}
-	if (cub->orientation == 'W')
-	{
-		cub->dir.x = -1;
-		cub->dir.y = 0;
-	}
 	if (cub->orientation == 'E')
 	{
 		cub->dir.x = 1;
@@ -34,32 +24,42 @@ static void		ft_initvdir(t_cub *cub)
 	if (cub->orientation == 'S')
 	{
 		cub->dir.x = 0;
+		cub->dir.y = 1;
+	}
+	if (cub->orientation == 'N')
+	{
+		cub->dir.x = 0;
 		cub->dir.y = -1;
+	}
+	if (cub->orientation == 'W')
+	{
+		cub->dir.x = -1;
+		cub->dir.y = 0;
 	}
 }
 
 static void		ft_initplane(t_cub *cub)
 {
 	cub->plane = malloc(sizeof(t_vector));
-	if (cub->orientation == 'N')
-	{
-		cub->plane->x = -((double)FOV / (double)100);
-		cub->plane->y = 0;
-	}
 	if (cub->orientation == 'E')
+	{
+		cub->plane->x = 0;
+		cub->plane->y = ((double)FOV / (double)100);
+	}
+	if (cub->orientation == 'N')
 	{
 		cub->plane->x = ((double)FOV / (double)100);
 		cub->plane->y = 0;
-	}
-	if (cub->orientation == 'W')
-	{
-		cub->plane->x = 0;
-		cub->plane->y = -((double)(FOV) / (double)100);
 	}
 	if (cub->orientation == 'S')
 	{
 		cub->plane->x = -((double)FOV / (double)100);
 		cub->plane->y = 0;
+	}
+	if (cub->orientation == 'W')
+	{
+		cub->plane->x = 0;
+		cub->plane->y = -((double)FOV / (double)100);
 	}
 }
 
